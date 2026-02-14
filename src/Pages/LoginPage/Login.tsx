b82007b50aeb7aa6462bf4798ad53b3695efd06d
Lukas <lukas.patrikc@yahoo.com>
2026-02-14 15:04:14 +0200

    Login done

--- src/Pages/LoginPage/Login.tsx
+++ src/Pages/LoginPage/Login.tsx
@@ -2,40 +2,64 @@ import "./Login.css";
 import { GiAirplaneDeparture } from "react-icons/gi";
 import { FaArrowRight } from "react-icons/fa6";
 import { FaLock } from "react-icons/fa6";
+import { MdOutlinePersonOutline } from "react-icons/md";
+import { MdAirplaneTicket } from "react-icons/md";
 
 export default function Login() {
   return (
     <div className="login">
+      <div className="header">
+        <GiAirplaneDeparture className="icon" />
+        <h1 className="title">
+          Your Face, Your <br /> Ticket.
+        </h1>
+        <p className="subtext">
+          Secure your seat with decentralized <br /> biometric identity. No
+          passports, no <br /> paper.
+        </p>
+      </div>
       <div className="container">
-        <div className="header">
-          <GiAirplaneDeparture className="icon" />
-          <h1 className="title">Your Face, Your Ticket.</h1>
-          <p className="subtext">
-            Secure your seat with decentralized biometric <br /> identity.No
-            passports, no paper.
-          </p>
-        </div>
         <div className="body">
           <form action="">
-            <label htmlFor=""> FULL LEGGAL NAME</label>
-            <input type="text" className="input" />
-            <label htmlFor=""> FLIGHT NUMBER</label>
-            <input type="text" className="input" />
-            <button type="submit">
-              Create Secure ID <FaArrowRight />
-            </button>
+            <div className="inp">
+              <label htmlFor="" className="inputLabel">
+                FULL LEGAL NAME
+              </label>
+              <div className="helper">
+                <MdOutlinePersonOutline className="formIcon" />
+                <input
+                  placeholder="Zbuce Andrei"
+                  type="text"
+                  className="input"
+                />
+              </div>
+            </div>
+            <div className="inp">
+              <label htmlFor="" className="inputLabel">
+                FLIGHT NUMBER
+              </label>
+              <div className="helper">
+                <MdAirplaneTicket className="formIcon" />
+                <input placeholder="RO 410" type="text" className="input" />
+              </div>
+            </div>
+            <div className="btn">
+              <button type="submit">
+                Create Secure ID <FaArrowRight />
+              </button>
+            </div>
           </form>
-          <div className="footer">
-            <div className="securityMessage">
-              <FaLock />
-              <p>Your data is encrypted and stored locally</p>
-            </div>
-            <div className="appStatus">
-              <div className="point"></div>
-              <p className="status">SYSTEM OPERATIONAL</p>
-            </div>
+        </div>
+        <div className="footer">
+          <div className="securityMessage">
+            <FaLock />
+            <p>Your data is encrypted and stored locally</p>
           </div>
         </div>
+      </div>
+      <div className="appStatus">
+        <div className="point"></div>
+        <p className="status">SYSTEM OPERATIONAL</p>
       </div>
     </div>
   );
